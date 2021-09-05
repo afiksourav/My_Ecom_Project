e388691a38325d37f75853551b88ab35eb0095e8
afiksourav <afiksourav1@gmail.com>
2021-09-05 19:55:44 +0600

    e

---
+++ App_Login/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+# Forms and Models
+from App_Login.models import Profile
+from App_Login.forms import ProfileForm, SignUpForm
+
+#messages
+from django.contrib import messages
+
+def sign_up(request):
+    form = SignUpForm()
+   # registered = False
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #registered = True
+            messages.success(request,"Account Created Successfully !")
+            return HttpResponseRedirect(reverse('App_Login:login'))
+    dict = {'form':form} #{'registered':registered}
+    return render(request, 'App_Login/sign_up.html', context=dict)
+
+def login_user(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('App_Shop:home'))
+    return render(request,'App_Login/login.html', context={'form':form})
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    messages.warning(request,"You are logged out!!")
+    return HttpResponseRedirect(reverse('App_Shop:home'))
+
+
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form =ProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Change Saved Successfully !")
+            form = ProfileForm(instance=profile)
+    return render(request,'App_Login/change_profile.html',context ={'form':form})
